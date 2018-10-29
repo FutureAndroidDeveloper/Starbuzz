0fc50cc625b3cda291974e15ece95d76e6e971e2
Kirill Klimenkov <dev.kirill.klimenkov@gmail.com>
2018-10-29 16:32:24 +0300

    Добавил Listener для ListView (list_drinks)

--- app/src/main/java/com/example/kirill/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/example/kirill/starbuzz/DrinkCategoryActivity.java
@@ -1,7 +1,10 @@
 package com.example.kirill.starbuzz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,9 +15,25 @@ public class DrinkCategoryActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
 
+        // Create adapter
         ArrayAdapter<Drink> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
 
-        ListView listView = (ListView) findViewById(R.id.list_drinks);
-        listView.setAdapter(adapter);
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(adapter);
+
+        // Create Listener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> ListView, View itemView, int position, long id) {
+                // Send drink, which user is picked
+                Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                startActivity(intent);
+            }
+        };
+
+        // Set Listener to ListView
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
